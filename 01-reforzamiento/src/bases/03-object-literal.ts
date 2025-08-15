e17bb3c255c139a969782983f38161955d245bc2
eperezm19 <erick.perez1927@gmail.com>
2025-08-14 23:39:59 -0600

    Refactor object literal definition and add cloning examples in 03-object-literal.ts; update main.ts to import arrays module

--- 01-reforzamiento/src/bases/03-object-literal.ts
+++ 01-reforzamiento/src/bases/03-object-literal.ts
@@ -1,4 +1,16 @@
-const person = {
+interface Person {
+  firstName: string;
+  lastName: string;
+  age: number;
+  address: Address;
+}
+
+interface Address {
+  country: string;
+  city: string;
+}
+
+const person: Person = {
   firstName: "Juan",
   lastName: "O'Neill",
   age: 20,
@@ -8,4 +20,20 @@ const person = {
   },
 };
 
+// Operador spread ... solo rompe la referencia, no crea una nueva instancia
+const person2 = { ...person };
+person2.firstName = "Juan";
+person2.address.city = "Los Angeles";
+person2.address.country = "United States";
+
 console.log({ person });
+console.log({ person2 });
+
+// Crear un clon de un objeto con una nueva referencia
+const person3 = structuredClone(person);
+person3.firstName = "Juan";
+person3.address.city = "Los Angeles";
+person3.address.country = "United States";
+
+console.log({ person });
+console.log({ person3 });
--- 01-reforzamiento/src/main.ts
+++ 01-reforzamiento/src/main.ts
@@ -1,6 +1,7 @@
 // import "./bases/01-const-let";
 // import "./bases/02-template-string";
-import "./bases/03-object-literal";
+// import "./bases/03-object-literal";
+import "./bases/04-arrays";
 
 document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
   <div>
