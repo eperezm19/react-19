1033d1bb299a0e264ec06bce7559fe98ca83c3a7
eperezm19 <erick.perez1927@gmail.com>
2025-08-14 23:50:31 -0600

    Enhance object literal examples in 03-object-literal.ts and update main.ts to include new functionality

---
+++ 01-reforzamiento/src/bases/05-functions.ts
@@ -0,0 +1,15 @@
+function greet(name: string) {
+  return `Hello ${name}`;
+}
+
+const greet2 = (name: string) => {
+  return `Hello ${name}`;
+};
+
+console.log(greet("John"));
+
+function add(a: number, b: number) {
+  return a + b;
+}
+
+console.log(add(1, 2));
