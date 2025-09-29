35153e2e6eb0b6a1fc87d21ef628502928db0c5e
Noel Perez <erick.perez1927@gmail.com>
2025-09-29 11:52:41 -0600

    Agrega soporte para la gestión de GIFs en GifsApp, incluyendo la obtención de GIFs mediante una consulta y la actualización de la lista de GIFs mostrada. Se eliminan los componentes GifList y PreviousSearches, y se reorganizan las importaciones para reflejar la nueva estructura de carpetas.

--- gifs-app/src/GifsApp.tsx
+++ gifs-app/src/GifsApp.tsx
@@ -1,11 +1,13 @@
 import { useState } from "react";
-import { GifList } from "./gifs/gif-list";
-import PreviousSearches from "./gifs/previous-searches";
-import { mockGifs } from "./mock-data/gifs.mock";
+import { GifList } from "./gifs/components/gif-list";
+import PreviousSearches from "./gifs/components/previous-searches";
 import CustomHeader from "./shared/components/custom-header";
 import SearchBar from "./shared/components/search-bar";
+import type { Gif } from "./gifs/interfaces/gif.interface";
+import { getGifsByQuery } from "./gifs/actions/get-gifs-by-query.action";
 
 export default function GifsApp() {
+  const [gifs, setGifs] = useState<Gif[]>([]);
   const [previousTerm, setpreviousTerm] = useState<string[]>([]);
 
   const handleTermClick = (term: string) => {
@@ -14,11 +16,13 @@ export default function GifsApp() {
     });
   };
 
-  const handleSearch = (query: string) => {
+  const handleSearch = async (query: string) => {
     const cleanQuery = query.trim().toLowerCase();
     if (cleanQuery === "") return;
     if (previousTerm.includes(cleanQuery)) return;
     setpreviousTerm([cleanQuery, ...previousTerm.slice(0, 8)]);
+    const gifs = await getGifsByQuery(cleanQuery);
+    setGifs(gifs);
   };
 
   return (
@@ -33,7 +37,7 @@ export default function GifsApp() {
       {/* Busquedas previas */}
       <PreviousSearches searches={previousTerm} onTermClick={handleTermClick} />
       {/* Gifs list */}
-      <GifList gifs={mockGifs} />
+      <GifList gifs={gifs} />
     </div>
   );
 }
--- gifs-app/src/gifs/components/gif-list.tsx
+++ gifs-app/src/gifs/components/gif-list.tsx
@@ -1,4 +1,4 @@
-import type { Gif } from "../mock-data/gifs.mock";
+import type { Gif } from "../../mock-data/gifs.mock";
 
 interface GifListProps {
   gifs: Gif[];
